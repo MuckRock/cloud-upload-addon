68a178acc5375c509a071bb1139ec563996e5861
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2024-02-16 06:52:37 -0600

    Handle APIError, DoesNotExistError

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 """ We use clouddl, a library to grab files from several file sharing sites
 and the DocumentCloud Add-On system"""
 import os
+import sys
 from clouddl import grab
 from documentcloud.addon import AddOn
+from documentcloud.exceptions import APIError, DoesNotExistError
+
 
 class Import(AddOn):
     """An Add-On that allows you to upload files into DocumentCloud from
@@ -10,14 +13,27 @@ class Import(AddOn):
 
     def main(self):
         """
-            Uses clouddl to grab documents from a Google Drive or Dropbox location, 
-            uploads them to DocumentCloud using upload_directory()
+        Uses clouddl to grab documents from a Google Drive or Dropbox location,
+        uploads them to DocumentCloud using upload_directory()
         """
+        try:
+            project = self.client.projects.get(self.data.get("project_id"))
+        except DoesNotExistError:
+            self.set_message("Invalid project ID specified. Try again")
+            sys.exit(0)
+        except APIError:
+            self.set_message("Invalid project ID specified. Try again")
+            sys.exit(0)
         os.makedirs(os.path.dirname("./out/"), exist_ok=True)
         url = self.data["url"]
         grab(url, "./out/")
-        project = self.client.projects.get(self.data.get("project_id"))
-        obj_list = self.client.documents.upload_directory("./out/", extensions=None, project=project.id, access=self.data.get("access_level"))
+        self.client.documents.upload_directory(
+            "./out/",
+            extensions=None,
+            project=project.id,
+            access=self.data.get("access_level"),
+        )
+
 
 if __name__ == "__main__":
     Import().main()
